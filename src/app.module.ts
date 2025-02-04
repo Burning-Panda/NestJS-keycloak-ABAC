418b7fdc2b6917e2342939361046dbbe9cc08bdb
André <31665764+Burning-Panda@users.noreply.github.com>
2025-02-04 19:10:33 +0100

    setup auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [UsersModule, AuthModule],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
